59d00392e6b6049abb83a5fb4220d945393a1301
sahilkesarwani <sahilkesarwani2@gmail.com>
2021-05-02 16:47:13 +0530

    Changes in am pm

--- Date_and_Time/js/script.js
+++ Date_and_Time/js/script.js
@@ -6,18 +6,18 @@ setInterval(() => {
     if(dateAndTime.getHours() == 12) {
         time = dateAndTime.getHours() + ':' + dateAndTime.getMinutes() + ':' + dateAndTime.getSeconds(); 
         date = dateAndTime.toLocaleDateString();
-        document.getElementById('time').innerHTML = time + ' pm ' + ' on ' + date;
+        document.getElementById('time').innerHTML = time + ' PM ' + ' on ' + date;
     }else if(dateAndTime.getHours() > 12) {
         time = (dateAndTime.getHours() - 12) + ':' + dateAndTime.getMinutes() + ':' + dateAndTime.getSeconds(); 
         date = dateAndTime.toLocaleDateString();
-        document.getElementById('time').innerHTML = time + ' pm ' + ' on ' + date;
+        document.getElementById('time').innerHTML = time + ' PM ' + ' on ' + date;
     }else if(dateAndTime.getHours() == 0) {
         time = (dateAndTime.getHours() + 12) + ':' + dateAndTime.getMinutes() + ':' + dateAndTime.getSeconds(); 
         date = dateAndTime.toLocaleDateString();
-        document.getElementById('time').innerHTML = time + ' am ' + ' on ' + date;
+        document.getElementById('time').innerHTML = time + ' AM ' + ' on ' + date;
     }else {
         time = dateAndTime.getHours() + ':' + dateAndTime.getMinutes() + ':' + dateAndTime.getSeconds(); 
         date = dateAndTime.toLocaleDateString();
-        document.getElementById('time').innerHTML = time + ' am ' + ' on ' + date;
+        document.getElementById('time').innerHTML = time + ' AM ' + ' on ' + date;
     }
 }, 1000);
