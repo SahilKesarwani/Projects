e0f7ab9c0462d3ac86d3f8cfc58aaace6424fb5f
sahilkesarwani <sahilkesarwani2@gmail.com>
2021-05-20 00:49:56 +0530

    Added Covid-19 live updates project

---
+++ Covid-19_updates/js/app.js
@@ -0,0 +1,35 @@
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://api.covid19india.org/data.json', true);
+
+xhr.onload = function () {
+    if(this.status === 200) {
+        let obj = JSON.parse(this.responseText);
+        let states = obj.statewise;
+        console.log(states);
+        
+        let statsHTML = ``;
+        let stats = '';
+
+        states.forEach((ele, ind) => {
+            if(ind >= 1) {
+                stats = `
+                    <tr>
+                        <td>${ele["lastupdatedtime"]}</td>
+                        <td>${ele["state"]}</td>
+                        <td>${ele["confirmed"]}</td>
+                        <td>${ele["active"]}</td>
+                        <td>${ele["deaths"]}</td>
+                        <td>${ele["recovered"]}</td>
+                    </tr>
+                    `;
+            }
+            statsHTML += stats;
+        });
+        document.getElementById('main-table').innerHTML += statsHTML;
+    }else {
+        console.error("Some Error Occured");
+    }
+}
+
+xhr.send();
